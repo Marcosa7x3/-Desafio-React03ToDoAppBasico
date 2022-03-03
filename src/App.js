8b335e65279a08e496718515b65b304aa5089e93
Marcos Adriano <marcosadriano645@gmail.com>
2022-03-03 15:42:02 -0300

    Desafio React 03 ToDo App Básico

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  var [lista, setLista] = useState([]);
+  var [novoItem, setNovoItem] = useState("");
+
+  useEffect(() => {
+    setLista([]);
+  }, []);
+  
+  return (
+    <div className="container">
+      <h1>LISTA</h1>
+      <div className="novo-item">
+        <input placeholder="Tarefa" value={novoItem} onChange={value => setNovoItem(value.target.value)} type="text"/>
+        <button onClick={() => adicionarNovoItem()}>Adicionar</button>
+      </div>
+      <ul className="todo-list">
+        {lista.map((item, index) => (
+          <li key={index} className="todo-item">
+            {item}
+            <button onClick={() => deletarItem(index)}>Deletar</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  function adicionarNovoItem () {
+    if (novoItem.length <= 0) {
+      alert("Por favor, digite algo nom campo 'Tarefa'");
+      return;
+    }
+
+    var itemIndex = lista.indexOf(novoItem);
+    if (itemIndex >= 0 ) {
+      alert("Você já adicionou essa tarefa.");
+      return;
+    }
+    
+    setLista([...lista, novoItem]);
+    setNovoItem("");
+  }
+
+  function deletarItem (index) {
+    var tmpArray =  [...lista];
+    tmpArray.splice (index, 1);
+
+    setLista (tmpArray);
+  }
+}
+
+export default App;
